ff0960e83f5a17498812a302be9d8855e96f167e
Nikita Domnickij <krevedkokun33@gmail.com>
2020-11-09 23:18:14 +0300

    fixed screen sharing in google meet on ungoogled-chromium

--- manifests/browser.scm
+++ manifests/browser.scm
@@ -1,7 +1,26 @@
-(specifications->manifest
- '(
-   "firefox"
-   "ungoogled-chromium"
-   ))
+(use-modules
+ (gnu packages)
+ (guix profiles)
+ (guix packages)
+ (guix utils)
+ (gnu packages chromium)
+ (nongnu packages mozilla))
+
+
+;; fixes screen-sharing in google-meet
+(define ungoogled-chromium-with-hangouts-services
+  (package/inherit
+   ungoogled-chromium
+   (arguments
+    (substitute-keyword-arguments
+     (package-arguments ungoogled-chromium)
+     ((#:configure-flags flags)
+      `(cons "enable_hangout_services_extension=true" ,flags))))))
+
+(packages->manifest
+ (list
+  firefox
+  ungoogled-chromium-with-hangouts-services
+  ))
 
 ;; guix package --profile=$GUIX_EXTRA_PROFILES/browser/browser --manifest=$HOME/.config/guix/manifests/browser.scm
