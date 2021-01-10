528766d1c6e1c71b2a8086062b68ac9aedf6b9be
Nikita Domnickij <krevedkokun33@gmail.com>
2021-01-10 23:49:16 +0300

    utilize options->transformations instead of inheriting packages

--- manifests/emacs.scm
+++ manifests/emacs.scm
@@ -5,31 +5,15 @@
  (guix packages)
  (guix git-download)
  (guix build-system emacs)
+ (guix transformations)
  ((guix licenses) #:prefix license:))
 
 
-(define emacs-evil-git
-  (package/inherit
-   emacs-evil
-   (source (origin
-             (method git-fetch)
-             (uri (git-reference
-                   (url "https://github.com/emacs-evil/evil")
-                   (commit "d6cf6680ec52733ea78dc530ed75fadc5171c758")))
-             (sha256
-              (base32 "14rl6jx7cj336raxbksh3r2cplyifz8dghdhqvf7h1ng10sd9j6b"))))))
-
-
-(define emacs-icomplete-vertical-git
-  (package/inherit
-   emacs-icomplete-vertical
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/oantolin/icomplete-vertical")
-                  (commit "a4c65f213bd3d8be94fe8cb28ecf7ff3b44405d1")))
-            (sha256
-             (base32 "02v190pb802vck7di39jyf5prvmfsgcxln8mgwsls2b4clx9da97"))))))
+(define transform
+  (options->transformation
+   '((with-commit . "emacs-evil=cc9d6886b418389752a0591b9fcb270e83234cf9")
+     (with-commit . "emacs-evil-collection=be07f6a2905494a97215fa236f3bf40f945dfcea")
+     (with-commit . "emacs-icomplete-vertical=0.3"))))
 
 
 (define emacs-flymake-posframe
@@ -108,77 +92,32 @@
     (home-page "https://github.com/minad/consult")))
 
 
-(define emacs-evil-surround'
-  (package/inherit
-   emacs-evil-surround
-   (propagated-inputs `(("emacs-evil" ,emacs-evil-git)))))
-
-(define emacs-evil-commentary'
-  (package/inherit
-   emacs-evil-commentary
-   (propagated-inputs `(("emacs-evil" ,emacs-evil-git)))))
-
-(define emacs-evil-multiedit'
-  (package/inherit
-   emacs-evil-multiedit
-   (propagated-inputs `(("emacs-evil" ,emacs-evil-git)
-			("emacs-iedit" ,emacs-iedit)))))
-
-(define emacs-evil-collection'
-  (package/inherit
-   emacs-evil-collection
-   (version "0.0.4")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/emacs-evil/evil-collection")
-                  (commit version)))
-            (sha256
-             (base32 "0ydr06krlj6v1r7hq3m5iy875mhmjr9amcfjjgqglfnzhic29g18"))))
-   (propagated-inputs `(("emacs-evil" ,emacs-evil-git)
-			("emacs-annalist" ,emacs-annalist)))))
-
-(define emacs-evil-cleverparens'
-  (package/inherit
-   emacs-evil-cleverparens
-   (propagated-inputs `(("emacs-evil" ,emacs-evil-git)
-			("emacs-paredit" ,emacs-paredit)
-			("emacs-smartparens" ,emacs-smartparens)))))
-
-(concatenate-manifests
- (list
-  (packages->manifest
-   (list emacs-evil-git
-         emacs-flymake-posframe
-         emacs-geiser-eros
-         emacs-icomplete-vertical-git
-         emacs-marginalia
-         emacs-consult
-	 emacs-evil-surround'
-         emacs-evil-multiedit'
-         emacs-evil-commentary'
-         emacs-evil-cleverparens'
-         emacs-evil-collection'))
-  (specifications->manifest
-   '("emacs-next"
-     "emacs-leaf"
-     ;; "emacs-evil-collection"
-     ;; "emacs-evil-cleverparens"
-     ;; "emacs-evil-commentary"
-     ;; "emacs-evil-multiedit"
-     ;; "emacs-evil-surround"
-     "emacs-orderless"
-     ;; "emacs-icomplete-vertical"
-     "emacs-modus-themes"
-     "emacs-geiser"
-     "emacs-which-key"
-     ;; "emacs-posframe"
-     "emacs-eros"
-     "emacs-gcmh"
-     "emacs-minions"
-     "emacs-magit"
-     "emacs-clojure-mode"
-     "emacs-cider"
-     "emacs-async"))))
+(packages->manifest
+ `(,emacs-geiser-eros
+   ,emacs-marginalia
+   ,emacs-consult
+   ,@(map specification->package
+          '("emacs-next"
+            "emacs-leaf"
+            "emacs-smartparens"
+            "emacs-orderless"
+            "emacs-modus-themes"
+            "emacs-geiser"
+            "emacs-which-key"
+            "emacs-eros"
+            "emacs-gcmh"
+            "emacs-minions"
+            "emacs-magit"
+            "emacs-clojure-mode"
+            "emacs-cider"
+            "emacs-async"))
+   ,@(map (compose transform specification->package)
+          '("emacs-evil"
+            "emacs-evil-collection"
+            "emacs-evil-cleverparens"
+            "emacs-evil-commentary"
+            "emacs-evil-multiedit"
+            "emacs-evil-surround"
+            "emacs-icomplete-vertical"))) )
 
 ;; guix package --profile=$GUIX_EXTRA_PROFILES/emacs/emacs --manifest=$HOME/.config/guix/manifests/emacs.scm
