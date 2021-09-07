4f43be853959f88ddf2380029951c7d96a5a9ce0
Nikita Domnitskii <nikita@domnitskii.me>
2021-09-07 23:47:41 +0300

    [wip] yaml serialization for alacritty

--- cfg/home/yggdrasil/terminals.scm
+++ cfg/home/yggdrasil/terminals.scm
@@ -4,9 +4,11 @@
   #:use-module (gnu home-services)
   #:use-module (gnu services configuration)
   #:use-module (gnu home-services configuration)
+  #:use-module (gnu home-services-utils)
   #:use-module (ice-9 match)
   #:use-module (guix gexp)
   #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-43)
   #:export (home-alacritty-configuration
             home-alacritty-service-type
             services))
@@ -14,36 +16,91 @@
 (define-configuration/no-serialization home-alacritty-configuration
   (package
     (package alacritty)
-    "")
+    "alacritty package to use")
   (config
    (alist '())
-   ""))
+   "Association list of key-value pair configuration.  The following configuration:
+@lisp
+(config
+ `((cursor . ((style . ((shape . Beam)))))
+   (font . ((normal . ((family . Iosevka)
+                       (style . Light)))
+            (size . 18.0)))))
+@end lisp
 
-(define (serialize-value value)
-  (if (string? value)
-      (format #f "'~a'" value)
-      value))
+would yield:
 
-(define* (serialize-yaml-config config #:key (depth 0))
-  (string-join
-   (append-map
-    (match-lambda
-      ((key . val)
-       (let ((tabs (make-string (* 2 depth) #\space)))
-         (if (list? val)
-             (list
-              (format #f "~a~a:" tabs key)
-              (serialize-yaml-config val #:depth (1+ depth)))
-             (list (format #f "~a~a: ~a" tabs key (serialize-value val)))))))
-    config)
-   "\n"))
+@example
+cursor:
+  style:
+    shape: Beam
+font:
+  normal:
+    family: Iosevka
+    style: Light
+  size: 18.0
+@end example"))
+
+(define (make-yaml-indent depth)
+  (make-string (* 2 depth) #\space))
+
+(define (serialize-yaml-vector-value depth value)
+  (let* ((depth (1+ depth))
+         (tab (make-yaml-indent depth)))
+    (cond
+     ((string? value) (format #f "~a- '~a'" tab value))
+     ((boolean? value) (format #f "~a- ~a" tab (if value "true" "false")))
+     ((alist? value) (string-join
+                      (cons
+                       (format #f "~a-" tab)
+                       (append-map (serialize-yaml-value (1+ depth)) value))
+                      "\n"))
+     ((vector? value) (string-join
+                       (cons
+                        (format #f "~a-" tab)
+                        (vector->list
+                         (vector-map (lambda (_ val)
+                                       (serialize-yaml-vector-value depth val))
+                                     value)))
+                       "\n"))
+     (else (format #f "~a- ~a" tab value)))))
+
+(define (serialize-yaml-value depth)
+  (match-lambda
+    ((key . value)
+     (let ((tab (make-yaml-indent depth)))
+       (cond
+        ((string? value) (list (format #f "~a~a: '~a'" tab key value)))
+        ((boolean? value) (list (format #f "~a~a: ~a" tab key (if value "true" "false"))))
+        ((alist? value) (cons
+                         (format #f "~a~a:" tab key)
+                         (append-map (serialize-yaml-value (1+ depth)) value)))
+        ((vector? value) (cons
+                          (format #f "~a~a:" tab key)
+                          (vector->list
+                           (vector-map (lambda (_ val)
+                                         (serialize-yaml-vector-value depth val))
+                                       value))))
+        (else (list (format #f "~a~a: ~a" tab key value))))))))
+
+(display
+ (serialize-yaml-alist
+  `((a . "#aaa")
+    (b . #t)
+    (c . 3)
+    (d . smth)
+    (h . #(1 #f "smth" a #(1 2 3) ((a . 1) (b . 2))))
+    (i . ((j . 1) (k . #f))))))
+
+(define* (serialize-yaml-alist value #:key (depth 0))
+  (string-join (append-map (serialize-yaml-value depth) value) "\n"))
 
 (define (add-alacritty-configuration config)
   (let ((cfg (home-alacritty-configuration-config config)))
     `(("config/alacritty/alacritty.yml"
        ,(mixed-text-file
          "alacritty.yml"
-         (serialize-yaml-config cfg))))))
+         (serialize-yaml-alist cfg))))))
 
 (define add-alacritty-package
   (compose list home-alacritty-configuration-package))
