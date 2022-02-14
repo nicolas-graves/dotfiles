cb3fe492ed4cc821a2ccacce8d564b0eccd53e03
Nicolas Graves <ngraves@ngraves.fr>
2022-02-15 00:37:14 +0100

    home: adding home.org to tangle configuration.

--- home/yggdrasil/pipewire.scm
+++
@@ -1,9 +0,0 @@
-(define-module (home yggdrasil pipewire)
-  #:use-module (gnu home services)
-  #:use-module (home services dbus)
-  #:use-module (home services pipewire))
-
-(define-public services
-  (list
-   (service home-dbus-service-type)
-   (service home-pipewire-service-type)))
