5b719089afaaa8d8b061a9dd31ce643ab9dc95e2
Nicolas Graves <ngraves@ngraves.fr>
2022-04-11 12:21:26 +0200

    home: biblatex cheatsheet.

--- home/yggdrasil/files/share/cheat/download.sh
+++ home/yggdrasil/files/share/cheat/download.sh
@@ -1,2 +1,3 @@
 wget "https://github.com/rstudio/cheatsheets/raw/master/rmarkdown-2.0.pdf"
 wget "https://www.andy-roberts.net/res/writing/latex/bibentries.pdf"
+wget "http://tug.ctan.org/info/biblatex-cheatsheet/biblatex-cheatsheet.pdf"
