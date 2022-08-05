f83f507656ae8c936329a1ace1baff77a13ca464
Nicolas Graves <ngraves@ngraves.fr>
2022-08-05 10:36:30 +0200

    package: adding guix-yasnippets.

--- home/packages/emacs.scm
+++ home/packages/emacs.scm
@@ -20,6 +20,7 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system emacs)
+  #:use-module (guix build-system copy)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define build-emacs emacs-next-pgtk-latest)
@@ -781,3 +782,23 @@ https://github.com/minad/consult/pull/173}.")
       (synopsis "magit-stgit")
       (description "magit-stgit")
       (license license:gpl3+))))
+
+(define-public guix-yasnippets
+  (package
+    (name "guix-yasnippets")
+    (version "0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://git.sr.ht/~ngraves/guix-yasnippets")
+             (commit "0520fb12a0a58f9d8ac4f16e8884dbcdd0e5f361")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1548xb6wad34h75x066l6dmhadpmf8shg4cfjcx2b5ila9zdb3m1"))))
+    (build-system copy-build-system)
+    (home-page "https://git.sr.ht/~ngraves/guix-yasnippets")
+    (synopsis "Development guix yasnippets")
+    (description "Development guix yasnippets. This repo has the purpose to
+migrate the exact same templates to skempo.")
+    (license license:gpl3+)))
