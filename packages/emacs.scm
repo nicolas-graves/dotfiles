c7907396e46cdfb993bffb08eda1371a57964e27
Nicolas Graves <ngraves@ngraves.fr>
2022-09-13 13:19:41 +0200

    deleting unused package emacs-org-roam-ui.

--- packages/emacs.scm
+++ packages/emacs.scm
@@ -24,37 +24,6 @@
   #:use-module (guix build-system copy)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define build-emacs emacs-next-pgtk-latest)
-
-(define-public emacs-org-roam-ui
-  (let ((commit "9474a254390b1e42488a1801fed5826b32a8030b")
-        (revision "0"))
-    (package
-      (name "emacs-org-roam-ui")
-      (version (git-version "0" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/org-roam/org-roam-ui")
-                      (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "0v54vxh95izch74wz2dl2dkdqicbvshra55l6qvd4xl5qmfhpjdc"))))
-      (build-system emacs-build-system)
-      (arguments
-       `(#:include (cons "^out" %default-include)
-                   #:emacs ,build-emacs))
-      (propagated-inputs
-       (list emacs-org-roam emacs-websocket emacs-simple-httpd emacs-f))
-      (home-page "https://github.com/org-roam/org-roam-ui")
-      (synopsis "A graphical frontend for your org-roam Zettelkasten")
-      (description " Org-Roam-UI is a frontend for exploring and interacting
-with your @code{org-roam} notes. It is meant a successor of
-@code{org-roam-server} that extends functionality of org-roam with a Web app
-that runs side-by-side with Emacs.")
-      (license license:gpl3+))))
-
 (define-public emacs-js-comint
   (package
     (name "emacs-js-comint")
