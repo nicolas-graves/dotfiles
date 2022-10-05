66518df6e1e6b55e6b7d4ed7307576a069ff3b67
Nicolas Graves <ngraves@ngraves.fr>
2022-10-05 22:11:19 +0200

    feature ssh: add option ssh-ssh-agent to start ssh-agent.

--- features/ssh.scm
+++ features/ssh.scm
@@ -2,10 +2,13 @@
   #:use-module (guix gexp)
   #:use-module (rde features)
   #:use-module (rde features predicates)
+  #:use-module (rde packages)
   #:use-module (gnu home-services ssh)
   #:use-module (gnu services)
   #:use-module (gnu services base)
   #:use-module (gnu services security-token)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shepherd)
   #:use-module (gnu packages)
   #:use-module (gnu packages ssh)
   #:use-module (packages cryptography)
@@ -21,10 +24,12 @@
 	  #:key
           (ssh openssh)
 	  (ssh-configuration (home-ssh-configuration))
-          (smart-card? #f))
+          (smart-card? #f)
+          (ssh-ssh-agent? #f))
   "Setup and configure SSH, ssh-agent, and take into account smart cards."
   (ensure-pred home-ssh-configuration? ssh-configuration)
   (ensure-pred boolean? smart-card?)
+  (ensure-pred boolean? ssh-ssh-agent?)
 
   (define (ssh-system-services config)
     (if smart-card?
@@ -40,8 +45,29 @@
 
   (define (ssh-home-services config)
     "Returns home services related to SSH."
-    (list (service home-ssh-service-type
-		   ssh-configuration)))
+    (append
+     (if ssh-ssh-agent?
+         (list
+          (simple-service
+           'start-ssh-agent-at-startup
+           home-shepherd-service-type
+           (list (shepherd-service
+                  (documentation "Run the ssh-agent at startup.")
+                  (provision '(ssh-agent))
+                  (requirement '())
+                  (start
+                   #~(make-forkexec-constructor
+                      (list #$(file-append
+                               (get-value 'ssh config)
+                               "/bin/ssh-agent")
+                            "-a" "/tmp/ssh-agent.socket" "-D")))
+                  (stop #~(make-kill-destructor)))))
+          (simple-service
+           'ssh-auth-export
+           home-environment-variables-service-type
+           `(("SSH_AUTH_SOCK" . "/tmp/ssh-agent.socket")))))
+     (list (service home-ssh-service-type
+		    ssh-configuration))))
 
   (feature
    (name 'ssh)
