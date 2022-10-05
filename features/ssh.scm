da9f8215ec947e35cc4e418553cd8d0cd2caae58
Nicolas Graves <ngraves@ngraves.fr>
2022-10-05 17:15:12 +0200

    import feature-ssh from RDE.

---
+++ features/ssh.scm
@@ -0,0 +1,31 @@
+(define-module (features ssh)
+  #:use-module (rde features)
+  #:use-module (rde features predicates)
+  #:use-module (gnu home-services ssh)
+  #:use-module (gnu services)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages ssh)
+
+  #:export (feature-ssh)
+
+  #:re-export (home-ssh-configuration
+	       ssh-host
+	       ssh-match))
+
+
+(define* (feature-ssh
+	  #:key
+          (ssh openssh)
+	  (ssh-configuration (home-ssh-configuration)))
+  "Setup and configure SSH, ssh-agent, and take into account smart cards."
+  (ensure-pred home-ssh-configuration? ssh-configuration)
+
+  (define (ssh-home-services config)
+    "Returns home services related to SSH."
+    (list (service home-ssh-service-type
+		   ssh-configuration)))
+
+  (feature
+   (name 'ssh)
+   (values `((ssh . ,openssh)))
+   (home-services-getter ssh-home-services)))
