12bb7c41231c750971b1f46cde90d92fd0b1287f
Nicolas Graves <ngraves@ngraves.fr>
2022-10-23 16:52:45 +0200

    feature ssh: update home-shepherd-service-type.

--- features/ssh.scm
+++ features/ssh.scm
@@ -21,9 +21,9 @@
 
 
 (define* (feature-ssh
-	  #:key
+          #:key
           (ssh openssh)
-	  (ssh-configuration (home-ssh-configuration))
+          (ssh-configuration (home-ssh-configuration))
           (ssh-agent? #f))
   "Setup and configure ssh and ssh-agent."
   (ensure-pred home-ssh-configuration? ssh-configuration)
@@ -33,27 +33,34 @@
     "Returns home services related to SSH."
     (append
      (if ssh-agent?
-         (list
-          (simple-service
-           'start-ssh-agent-at-startup
-           home-shepherd-service-type
-           (list (shepherd-service
-                  (documentation "Run the ssh-agent at startup.")
-                  (provision '(ssh-agent))
-                  (requirement '())
-                  (start
-                   #~(make-forkexec-constructor
-                      (list #$(file-append
-                               (get-value 'ssh config)
-                               "/bin/ssh-agent")
-                            "-a" "/tmp/ssh-agent.socket" "-D")))
-                  (stop #~(make-kill-destructor)))))
-          (simple-service
-           'ssh-auth-export
-           home-environment-variables-service-type
-           `(("SSH_AUTH_SOCK" . "/tmp/ssh-agent.socket")))))
+         (let* ((runtime (or (getenv "XDG_RUNTIME_DIR")
+                             (format #f "/run/user/~a" (getuid))))
+                (sock (string-append runtime "/ssh-agent.sock")))
+           (list
+            (simple-service
+             'start-ssh-agent-at-startup
+             home-shepherd-service-type
+             (list (shepherd-service
+                    (documentation "Run the ssh-agent at startup.")
+                    (provision '(ssh-agent))
+                    (requirement '())
+                    (start
+                     #~(make-forkexec-constructor
+                        (list (string-append
+                                 #$(get-value 'ssh config)
+                                 "/bin/ssh-agent")
+                              "-d" "-a" #$sock)
+                        #:log-file (string-append
+                                    (or (getenv "HOME")
+                                        (format #f "/home/~a" (getlogin)))
+                                    "/.local/var/log/ssh-agent.log")))
+                    (stop #~(make-kill-destructor)))))
+            (simple-service
+             'ssh-auth-socket-env-export
+             home-environment-variables-service-type
+             `(("SSH_AUTH_SOCK" . ,sock))))))
      (list (service home-ssh-service-type
-		    ssh-configuration))))
+                    ssh-configuration))))
 
   (feature
    (name 'ssh)
