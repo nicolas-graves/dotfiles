c47d63e8437574ede0f63a4e8f194196ab9f93db
Nicolas Graves <ngraves@ngraves.fr>
2023-02-10 17:22:48 +0100

    cleanup files, using origin instead of package.

--- files.scm
+++ files.scm
@@ -5,115 +5,22 @@
   #:use-module (guix download)
   #:use-module (guix git-download))
 
-(define-public nonguix-key
-  (package
-    (name "nonguix-key")
-    (version "0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri "https://substitutes.nonguix.org/signing-key.pub")
-       (sha256
-        (base32 "0j66nq1bxvbxf5n8q2py14sjbkn57my0mjwq7k1qm9ddghca7177"))))
-    (build-system copy-build-system)
-    (home-page "https://gitlab.com/nonguix/nonguix")
-    (synopsis "Key for nonguix substitutes")
-    (description "Signing key for nonguix substitutes")
-    (license license:cc0)))
-
-(define-public bg
-  (package
-    (name "bg")
-    (version "0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri "https://pour-un-reveil-ecologique.org/media/images/fond_pre.original.jpg")
-       (file-name "fond_pre.jpg")
-       (sha256
-        (base32 "03rn4fw9j31s7hl635q872hzxj4bj5m9hkjd4iqzl8z4lk0n9iiy"))))
-    (build-system copy-build-system)
-    (home-page "https://pour-un-reveil-ecologique.org/")
-    (synopsis "My background image")
-    (description "My background image")
-    (license license:cc0)))
-
-(define-public bg-lock
-  (package
-    (name "bg-lock")
-    (version "0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri "https://pour-un-reveil-ecologique.org/media/images/fond_lock_pre.original.jpg")
-       (file-name "fond_lock_pre.jpg")
-       (sha256
-        (base32 "1cyvaj0yvy6zvzy9yf1z6i629rwjcq3dni01phb599sp4n2cpa8g"))))
-    (build-system copy-build-system)
-    (home-page "https://pour-un-reveil-ecologique.org/")
-    (synopsis "My background image for lockscreen")
-    (description "My background image for lockscreen")
-    (license license:cc0)))
-
-(define-public base16-waybar
-  (let* ((commit "d2f943b1abb9c9f295e4c6760b7bdfc2125171d2")
-         (revision "0"))
-    (package
-      (name "base16-waybar")
-      (version (git-version "0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/mnussbaum/base16-waybar")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "1kc4w98rbdb3lyqrf8lzc1xsgn1zxqlzwxry6fiz89ygn84gh5ks"))))
-      (build-system copy-build-system)
-      (home-page "https://github.com/mnussbaum/base16-waybar")
-      (synopsis "Base16 CSS for waybar")
-      (description "Base16 CSS color and templates for waybar.")
-      (license license:expat))))
-
 (define-public programmer-beop
-  (let* ((commit "402305021b7fbf825aa4ea8381f4c5ae9a5ed81f")
-         (revision "0"))
-    (package
-      (name "programmer-beop")
-      (version (git-version "0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/luxcem/programmer-beop")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "1y968pd3ynjngvwr1zkcwkvhkwblzs3isdak12zqzvgl1krdhqhv"))))
-      (build-system copy-build-system)
-      (home-page "https://xn--nxa.luxcem.fr/Programmer-beop/")
-      (synopsis "Disposition de clavier ergonomique basé sur Bépo")
-      (description "Disposition de clavier ergonomique basé sur Bépo")
-      (license license:gpl3))))
+  (let ((commit "402305021b7fbf825aa4ea8381f4c5ae9a5ed81f"))
+    (origin
+      (method git-fetch)
+      (uri (git-reference
+            (url "https://github.com/luxcem/programmer-beop")
+            (commit commit)))
+      (sha256
+       (base32 "1y968pd3ynjngvwr1zkcwkvhkwblzs3isdak12zqzvgl1krdhqhv")))))
 
 (define-public programmer-beop-image
-  (package
-    (name "programmer-beop-image")
-    (version "0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri "https://luxcem.github.io/images/programmer_beop/prbeop.png")
-       (file-name "prbeop.png")
-       (sha256
-        (base32 "10dzfwm75b7p3jk0i6hi3wkpjyfbj9jsx78zxzmm0cippphnpzw9"))))
-    (build-system copy-build-system)
-    (home-page "https://xn--nxa.luxcem.fr/Programmer-beop/")
-    (synopsis "Disposition de clavier ergonomique basé sur Bépo")
-    (description "Disposition de clavier ergonomique basé sur Bépo -
-layout image (not saved)")
-    (license license:gpl3)))
+  (origin
+    (method url-fetch)
+    (uri "https://luxcem.github.io/images/programmer_beop/prbeop.png")
+    (sha256
+     (base32 "10dzfwm75b7p3jk0i6hi3wkpjyfbj9jsx78zxzmm0cippphnpzw9"))))
 
 (define-public vosk-model-small-fr
   (package
