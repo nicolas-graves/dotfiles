f57b5577ea88452931e46c7f2e40c50ed3f9e8ef
Nicolas Graves <ngraves@ngraves.fr>
2023-09-21 12:45:43 +0200

    Merging repo guix-template into this one.
    This commit has been produced in the following environment:
    /home/graves/spheres/info/rde:8f76cbbcf7efafa96f3acd3908955708dcb24a1d
    /home/graves/spheres/info/nonguix:e951cda078529d238f1bc942db1112f76a01a5f7
    /home/graves/spheres/info/guix:7252a932e09a6ebbb6127a2e8eaab0056852eaba

---
+++ template/channels.scm
@@ -0,0 +1,9 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit "1984d56b0e437af7be7fa6cf8e1a00e45eb8ffa1")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
