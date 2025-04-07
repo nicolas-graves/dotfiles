340c920b416f8500040a444accf7f05f4446563c
Nicolas Graves <ngraves@ngraves.fr>
2025-04-07 07:30:01 +0200

    config: Update nonguix/package-input-grafting.

--- configuration.scm
+++ configuration.scm
@@ -913,7 +913,9 @@ PACKAGE when it's not available in the store.  Note that this procedure calls
      ;; see https://github.com/NVIDIA/open-gpu-kernel-modules/issues/472
      (service (@ (nongnu services nvidia) nvidia-service-type)
               ((@ (nongnu services nvidia) nvidia-configuration)
-               (driver (@@ (nongnu packages nvidia) mesa/fake-beta))
+               (driver ((@ (nonguix utils) package-input-grafting)
+                        `((,(@ (gnu packages gl) mesa)
+                           . ,(@ (nongnu packages nvidia) nvdb)))))
                (firmware (@ (nongnu packages nvidia) nvidia-firmware-beta))
                (module (@ (nongnu packages nvidia) nvidia-module-beta))))
      (simple-service 'nvidia-mesa-utils-package
